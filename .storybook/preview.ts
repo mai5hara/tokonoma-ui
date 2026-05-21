d13fee8d04109fee8e669748bbf4f79633c96fdd
mai5hara <ishiyakiimo89@gmail.com>
2026-05-20 17:21:15 -0700

    Add color tokens and palettes for design system
    - Introduced color tokens and utility functions for managing color themes in the design system.
    - Created a ColorPalette component to display color families and their respective shades.
    - Updated global styles to utilize CSS variables for colors, enhancing theming capabilities.
    - Modified Storybook stories to reflect new component structure and organization.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,14 @@
 import type { Preview } from '@storybook/react-vite'
 
+import '../src/index.css'
+
 const preview: Preview = {
   parameters: {
+    layout: 'fullscreen',
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
